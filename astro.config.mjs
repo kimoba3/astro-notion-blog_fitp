261d422ba4b753bfa9d7ad910a5c31460c4dfa19
きむおば <contact@kimoba.com>
2023-06-05 07:59:09 +0900

    Revert "Feature/merge otoyo20230602"

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,29 +6,25 @@ import FeaturedImageDownloader from './src/integrations/featured-image-downloade
 import PublicNotionCopier from './src/integrations/public-notion-copier';
 
 const getSite = function () {
+  if (!process.env.CF_PAGES) {
+    return new URL(BASE_PATH, 'http://localhost:3000').toString();
+  }
+
+  if (process.env.CF_PAGES_BRANCH !== 'main') {
+    return new URL(BASE_PATH, process.env.CF_PAGES_URL).toString();
+  }
+
   if (CUSTOM_DOMAIN) {
     return new URL(BASE_PATH, `https://${CUSTOM_DOMAIN}`).toString();
   }
 
-  if (process.env.VERCEL && process.env.VERCEL_URL) {
-    return new URL(BASE_PATH, `https://${process.env.VERCEL_URL}`).toString();
-  }
-
-  if (process.env.CF_PAGES) {
-    if (process.env.CF_PAGES_BRANCH !== 'main') {
-      return new URL(BASE_PATH, process.env.CF_PAGES_URL).toString();
-    }
-
-    return new URL(
-      BASE_PATH,
-      `https://${new URL(process.env.CF_PAGES_URL).host
-        .split('.')
-        .slice(1)
-        .join('.')}`
-    ).toString();
-  }
-
-  return new URL(BASE_PATH, 'http://localhost:3000').toString();
+  return new URL(
+    BASE_PATH,
+    `https://${new URL(process.env.CF_PAGES_URL).host
+      .split('.')
+      .slice(1)
+      .join('.')}`
+  ).toString();
 };
 
 // https://astro.build/config
